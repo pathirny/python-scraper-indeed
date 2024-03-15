3d05db498da1d185c1eb6da764a3bdd0b3bd73bb
Patryk Hirny <pathirny@googlemail.com>
2024-03-15 22:51:20 +0000

    get amount of jobs

--- scape.py
+++ scape.py
@@ -1,14 +1,30 @@
-import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
-
-Headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
-url = "https://uk.indeed.com/jobs?q=junior+software+engineer&l=London%2C+Greater+London&from=searchOnHP&vjk=6f2091c334c10398"
-
-response = requests.get(url, headers=Headers)
-print(response.status_code)
-
+import time
+import random
+# set job title and location
+start = time.time()
+sleep = time.sleep(random.randint(2,6))
+job_title = "Junior+Software+Engineer"
+location = "London"
+# run in Incognito mode
+option = webdriver.ChromeOptions()
+option.add_argument("--incognito")
+# set the link
+url = f"https://uk.indeed.com/m/jobs?q={job_title}&radius=25&filter=0&l={location}"
+# look at the page 
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
+driver.get(url.format(job_title, location,0))
+# get the amount of jobs for the search
+amount_of_jobs = driver.find_element(By.CLASS_NAME, 'jobsearch-JobCountAndSortPane-jobCount').text;
+# get amount of jobs per page to iterate over pages
+max_pages = int(amount_of_jobs.split(' ')[0])//15
+driver.quit()
+end = time.time()
+print(end - start, "seconds to complete search")
+print("-------------------")
+print("Amount of jobs: ", amount_of_jobs)
+print("Max amount of pages for this search: ", max_pages)
