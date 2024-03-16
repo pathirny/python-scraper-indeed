28b2aa927608457c03cd0f573052a76ab34bc30f
Patryk Hirny <pathirny@googlemail.com>
2024-03-16 17:18:51 +0000

    scraping job page

--- scape.py
+++ scape.py
@@ -7,22 +7,33 @@ import time
 import random
 # set job title and location
 start = time.time()
-sleep = time.sleep(random.randint(2,6))
 job_title = "Junior+Software+Engineer"
 location = "London"
+## data I want to collect
+salary = []
+job_description = []
+job_list = []
+
+# look at the page 
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
+
 # run in Incognito mode
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 # set the link
 url = f"https://uk.indeed.com/m/jobs?q={job_title}&radius=25&filter=0&l={location}"
-# look at the page 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
-driver.get(url.format(job_title, location,0))
-# get the amount of jobs for the search
+# # get the amount of jobs for the search
 amount_of_jobs = driver.find_element(By.CLASS_NAME, 'jobsearch-JobCountAndSortPane-jobCount').text;
-# get amount of jobs per page to iterate over pages
+# # get amount of jobs per page to iterate over pages
 max_pages = int(amount_of_jobs.split(' ')[0])//15
-driver.quit()
+for i in range(0, max_pages):
+    driver.get(url.format(job_title, location,i* 10))
+    sleep = time.sleep(random.randint(2, 4))
+
+    job_page = driver.find_element(By.ID, "mosaic-jobResults")
+
+# driver.quit()
+
 end = time.time()
 print(end - start, "seconds to complete search")
 print("-------------------")
