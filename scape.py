4e9005d30fa4131f7feb95db5e0ac28081ec9432
Patryk Hirny <pathirny@googlemail.com>
2024-03-24 18:26:24 +0000

    creating dict but data isnt an exact match

--- scape.py
+++ scape.py
@@ -87,16 +87,16 @@ attempt_list = []
 # print(job_list)
 
 jobListCopied = [['Software Engineer - Modelling and Simulation', 'https://uk.indeed.com/pagead/clk?mo=r&ad=-6NYlbfkN0AoucB_G_SJpE8RPm3FmLy-hUK9s8ytG_pFzAFTGy7o3pcbijiII1urEfGnHiHUKFT1FvtnInOWYu0_fk87zYAD-5siKYekeyh-YnZk3TN77_AC_2ppjr4wzjxDDRZqCIpZV0bTMdnAovaHkIi2hZwpVj9BoDmDFeNrwZDKOaUVcYzJOXckqtq9_SRNsJwG3ie16VxJYFWEO65IZwNKT1_9tcHXWKp5Fvf9ZwV1Vd559AhMdcZs_m8PEIIYISl5JQjcKytgBrpfL8Q1phkaZ5By4YZeDrpcyzdlwbMO3v_N9cOF17p52liDwiLGHwFAQc5kCG5e7vD_Q6sFE1rwge_FIgTtIZdcmiyNvj3GpC6qz9GOLeULrEK_Dc5wmcfLlEQUd45A8W9jGjsoXWlzMVQRFsn1jfSubIdBhfmshza2cTu3wjPtCuoeki7BfSI1BCVy6Uily3LAhN_v-6o4ttI71L65vzbW_2k9A15OpMZsL9hqQbFi01RWobQRXAUf_YoVB6KyOoNCWjqBz6-JhNEVgqrH4t3OBWFzhos0Hvz2cF6pkDdVUeMMmRAv-wEgE3Rpj0Z1ekY-sG2ezyzK5KGTtsTS8Pi5tIxMLd7R8IFIrTfqhfTzgHdoFkY05N_6u2qjE4Yrukwi9P8KGgt6eHPVLDhQzwilT0qSJNrTSxwHtt-7pTIDl-pZGIzufKoGKROspyWVF9-cCiyavi5gWi4kdPiaJB6vEJOeWt2OkctzhIe7wquaN6LSaQOiM84iMciu1skb17rubN3zMJlVPZG9RylkLrdKnMBH9_q6ofuRFk6ByWzLwFSMOqrAjy2qcM8q3cqfB9lj9Q==&xkcb=SoA86_M3D471YIgI_r0LbzkdCdPP&camk=4HOcmqOLYrBhPnBb-cy5xQ==&p=0&fvj=0&vjs=3', 'sj_6f2091c334c10398', 'Northrop Grumman', 'Hybrid remote in London'], ['Lead Core Java Developer', 'https://uk.indeed.com/rc/clk?jk=e4195132078cf1b5&bb=fD2PxZfr3Bv_NyrO_71u-Z5MSgFTQWRKyuJem0oARmDkWElt-5a7N5jPrEWgMvzy5MxxQnWb9FddzfZjNLXfhwzuQR-SwmCknwSCcybqzYuNX8fobSSsWw%3D%3D&xkcb=SoAo67M3D47wNrAI_L0GbzkdCdPP&fccid=9353252f275fbb30&vjs=3', 'job_e4195132078cf1b5', 'Warner Bros. Discovery', 'London W4'], ['Principal Python Engineer - Core (Remote)', 'https://uk.indeed.com/rc/clk?jk=3cd4223b21688df5&bb=fD2PxZfr3Bv_NyrO_71u-WlPVLwfgc4OilCZb_FsP-tu0uCZNecWgFUK4uANuy1NICO222ptUTMutaaP080fHxuEhxqI6d-tYnjPTScIQNWvRRdJoEiRPg%3D%3D&xkcb=SoC167M3D47wNrAI_L0FbzkdCdPP&fccid=93143575209a3ce4&vjs=3', 'job_3cd4223b21688df5', 'Warner Bros. Discovery', 'London W4']]
-for key in fields:
 #     #print(key)
 #### value in jobListCopied gets individual lists in the ist(array)
-    for value in jobListCopied:
-        print(value)
+for value in jobListCopied:
+    print(value)
+    for key in fields:
         for i in value:
-            jobListCopied.remove(value)
             result[key] = i
-            break
-#cprint(result)
+            value.remove(i)
+    
+print(result)
 ########## each job return a array which contains all the info in an array, will need another for loop to iterate over each element?
 # create api to visualise the data
 #print(result)
