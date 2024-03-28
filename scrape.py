b02f3f114ae052b9c962d5f5fd46c17278a256f9
Patryk Hirny <pathirny@googlemail.com>
2024-03-28 12:13:21 +0000

    attempting diffrerent methods

--- scrape.py
+++ scrape.py
@@ -70,12 +70,14 @@ with open('jobs.csv', 'w', newline='') as csvfile:
     jobwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
     #jobwriter.writerows(fields)
     jobwriter.writerow(fields)
+    jobwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+
     for word in job_list:
         jobwriter.writerows([word])
-        jobwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
         jobwriter.writerows(job_list)
-        for pay in salary:
-            jobwriter.writerows(salary)
+    for pay in salary:
+        jobwriter.writerows(salary)
+       
 
 # convert the 2 list (fields and job_list) into dictionary
 result = []
