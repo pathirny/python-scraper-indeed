1b13cc2619d8b582e0058cbe00f357c8761fa140
Patryk Hirny <pathirny@googlemail.com>
2024-04-03 14:26:30 +0100

    improved search time by around 10s by removing unnecessary page reloads

--- scrape.py
+++ scrape.py
@@ -85,7 +85,6 @@ driver.quit()
 end = time.time()
 
 print(end - start, "seconds to complete search")
-print("-------------------")
 print("Amount of jobs: ", amount_of_jobs)
 print("Max amount of pages for this search: ", max_pages)
 
@@ -98,7 +97,6 @@ for job, sal in zip(job_list, salary):
 
 json_result = json.dumps(result)
 
-print(result)
 
 field_names = list(fields)
 with open('jobs.csv', 'w', newline='') as csvfile:
