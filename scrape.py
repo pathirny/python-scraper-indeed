41f8de44720e6cbb6ebbb3b187f63a6202e4a7bb
Patryk Hirny <pathirny@googlemail.com>
2024-04-06 17:44:13 +0100

    job seen

--- scrape.py
+++ scrape.py
@@ -49,6 +49,7 @@ for i in range(max_pages):
     # have to verify that program is human
     # this gets the mosaicResults
     job_page = driver.find_element(By.ID, "mosaic-jobResults")
+    # all jobs have a job_seen_beacon
     jobs = job_page.find_elements(By.CLASS_NAME, "job_seen_beacon")
     # iterate over the results (jobs) - iterating over each job 
     for j in jobs:
